ad180ee8198719f4a4979fbdaa98fb93fad2268b
aschreff <rainbow-andrew@hotmail.com>
2023-01-02 11:00:29 -0500

    start of translation copying for book 1

--- src/components/global/CustomFooter.js
+++ src/components/global/CustomFooter.js
@@ -11,7 +11,7 @@ const { Footer } = Layout;
  */
 const CustomFooter = () => {
   return (
-    <StyledFooter>Developed by Andrew Schreffler, Version 1.0.6</StyledFooter>
+    <StyledFooter>Developed by Andrew Schreffler, Version 1.1.0</StyledFooter>
   );
 };
 
