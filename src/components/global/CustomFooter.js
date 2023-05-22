b73aa3ba1670caa8f6261023ad84e2836525d62a
Andrew Schreffler <andrew.schreffler@shiftsmart.com>
2023-05-22 17:44:29 -0400

    more book 3 translation content

--- src/components/global/CustomFooter.js
+++ src/components/global/CustomFooter.js
@@ -11,7 +11,7 @@ const { Footer } = Layout;
  */
 const CustomFooter = () => {
   return (
-    <StyledFooter>Developed by Andrew Schreffler, Version 1.3.1</StyledFooter>
+    <StyledFooter>Developed by Andrew Schreffler, Version 1.3.2</StyledFooter>
   );
 };
 
