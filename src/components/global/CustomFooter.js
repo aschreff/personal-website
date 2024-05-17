b0690662a0f4191d253d7b9a69a6b4a71b751e29
Andrew Schreffler <andrew.schreffler@shiftsmart.com>
2024-05-17 13:49:48 -0400

    book 3 finished

--- src/components/global/CustomFooter.js
+++ src/components/global/CustomFooter.js
@@ -11,7 +11,7 @@ const { Footer } = Layout;
  */
 const CustomFooter = () => {
   return (
-    <StyledFooter>Developed by Andrew Schreffler, Version 1.3.10</StyledFooter>
+    <StyledFooter>Developed by Andrew Schreffler, Version 1.3.11</StyledFooter>
   );
 };
 
